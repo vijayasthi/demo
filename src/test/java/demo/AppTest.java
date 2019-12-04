9cfb4313cba165dfe4c37b4a0b1457fa441726da
vijayasthi <57709363+vijayasthi@users.noreply.github.com>
2019-12-04 12:18:40 +0100

    Update AppTest.java

--- src/test/java/demo/AppTest.java
+++ src/test/java/demo/AppTest.java
@@ -11,4 +11,9 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+    
+    @Test public void testContentAppAGreeting() {
+        App classUnderTest = new App();
+		assertEquals("Hi Hello world.", classUnderTest.getGreeting();
+    }
 }
