efe6d32817cd6da62013a4d5f0c55ce1f0e65a71
vijayasthi <57709363+vijayasthi@users.noreply.github.com>
2019-12-09 16:07:54 +0100

    Update App.java

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -10,6 +10,5 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        System.out.println(new App().getGreeting());
     }
 }
