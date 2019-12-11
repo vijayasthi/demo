06c9d03130aa2efecd7df1943e56da127f52af09
vijayasthi <57709363+vijayasthi@users.noreply.github.com>
2019-12-11 15:03:36 +0100

    Create CreateFiles.java

---
+++ src/main/java/demo/CreateFiles.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class CreateFiles {
+
+    public static void main(String[] args) {
+        try{
+            // Create new file
+            String content = "This is the content to write into create file";
+            String path="C:\Users\vasthi\Desktop\hi.txt";
+            File file = new File(path);
+
+            // If file doesn't exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            // Write in file
+            bw.write(content);
+
+            // Close connection
+            bw.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
