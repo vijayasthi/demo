863855756b940002a802585da2b59cf6b36d7c04
vijayasthi <57709363+vijayasthi@users.noreply.github.com>
2019-12-12 12:42:33 +0100

    Update FileTest.java

--- src/test/java/demo/FileTest.java
+++ src/test/java/demo/FileTest.java
@@ -7,6 +7,6 @@ public class FileTest {
     @Test public void testFileisCreated() {
         //File file = new File("C:/Users/vasthi/Desktop/hi.txt");
         //assertTrue(file.exists());
-        assertTrue(Paths.get("C:/Users/vasthi/Desktop/hi.txt").toFile().exists())
+        assertTrue(Paths.get("C:/Users/vasthi/Desktop/hi.txt").toFile().exists());
     }
 }
