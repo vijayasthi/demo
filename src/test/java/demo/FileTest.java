e4cf539c063235cd9211490c2e3b02ba775e3460
vijayasthi <57709363+vijayasthi@users.noreply.github.com>
2019-12-12 13:06:47 +0100

    Update FileTest.java

--- src/test/java/demo/FileTest.java
+++ src/test/java/demo/FileTest.java
@@ -10,7 +10,7 @@ public class FileTest {
         //assertTrue(Paths.get("C:/Users/vasthi/Desktop/hi.txt").toFile().exists());
         //Path p = Paths.get("C:/Users/vasthi/Desktop/hi.txt");
         //assertTrue(Files.exists(p));
-        assertTrueFiles.exists(Paths.get("C:/Users/vasthi/Desktop/hi.txt"));
+        assertTrue(Files.exists(Paths.get("C:/Users/vasthi/Desktop/hi.txt")));
         
         //if(Files.exists(Paths.get("C:/Users/vasthi/Desktop/hi.txt"))) { 
           //  assertTrue;
